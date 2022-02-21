e4f93db9fc546831276b65acc5efd4364719c343
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-02-21 22:44:10 +0100

    styles header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,79 @@
 import React from "react";
 import s from "./Header.module.scss";
+import SvgIcon from "../../UI/SvgIcon/SvgIcon";
 
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className={s.header}>
-      <div className={s.logo}>ADVENTURER</div>
-      <nav>
-        <ul className={s.nav}>
-          <li className={s.item}>
-            <NavLink to="/product">PRODUCT</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to="/sales">SALES</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to="/contacts">CONTACTS</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to="/support">SUPPORT</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div></div>
-    </div>
+    <>
+      <div className={s.header}>
+        <div className={s.logo}>
+          <div className={s.logoIconWrapper}>
+            <SvgIcon iconName="icon-logo" className={s.icon} />
+          </div>
+          <div className={s.logoText}>Adventurer</div>
+        </div>
+        <nav>
+          <ul className={s.nav}>
+            <li className={s.item}>
+              <NavLink
+                to="/product"
+                className={({ isActive }) => {
+                  return isActive ? s.navLinkActive : s.navLink;
+                }}
+              >
+                <p className={s.text}>Product</p>
+              </NavLink>
+            </li>
+            <li className={s.item}>
+              <NavLink
+                to="/sales"
+                className={({ isActive }) => {
+                  return isActive ? s.navLinkActive : s.navLink;
+                }}
+              >
+                <p className={s.text}>Sales</p>
+              </NavLink>
+            </li>
+            <li className={s.item}>
+              <NavLink
+                to="/contacts"
+                className={({ isActive }) => {
+                  return isActive ? s.navLinkActive : s.navLink;
+                }}
+              >
+                <p className={s.text}>Contacts</p>
+              </NavLink>
+            </li>
+            <li className={s.item}>
+              <NavLink
+                to="/support"
+                className={({ isActive }) => {
+                  return isActive ? s.navLinkActive : s.navLink;
+                }}
+              >
+                <p className={s.text}>Support</p>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={s.cart}>
+          <div className={s.langToggler}>
+            <p className={s.langText}>En</p>
+            <div className={s.languageIconWrapper}>
+              <SvgIcon iconName="icon-arrow-down" className={s.icon} />
+            </div>
+          </div>
+          <div className={s.cartIconWrapper}>
+            <SvgIcon iconName="icon-cart" className={s.icon} />
+          </div>
+        </div>
+      </div>
+      <div className={s.line}>
+        {/* <SvgIcon iconName="icon-product-line" className={s.icon} /> */}
+      </div>
+    </>
   );
 };
 

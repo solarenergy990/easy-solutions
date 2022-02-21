2da8a99344dce0611afcd40c3a561fd1151c7104
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-02-21 08:54:03 +0100

    App update

---
+++ src/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div></div>
+      <nav>
+        <ul>
+          <li>PRODUCT</li>
+          <li>SALES</li>
+          <li>CONTACTS</li>
+          <li>SUPPORT</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;

8067025b1999d0224d29099d1a17f728611f04a5
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-02-23 21:04:23 +0100

    about section complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,6 +14,33 @@ const About = () => {
         <div className={s.circleSeat}></div>
         <div className={s.circleStand}></div>
         <div className={s.mainChair}></div>
+        <div className={s.seatStandDashes}></div>
+        <div className={s.backDashes}></div>
+        <div className={s.pillowDashes}></div>
+        <div className={s.biggerPillow}>
+          <p className={s.pillowContent}>
+            Upgraded large neck pillow made with memory foam to bring our
+            customers the most comfortable office chair for gaming and working.
+          </p>
+        </div>
+        <div className={s.biggerBack}>
+          <p className={s.backContent}>
+            Our designers are creating the perfect high back ergonomic gaming
+            chair.
+          </p>
+        </div>
+        <div className={s.biggerSeat}>
+          <p className={s.seatContent}>
+            Luxury leather not only outshine the regular gaming computer chairs,
+            but also bring you the ultimate comfort.
+          </p>
+        </div>
+        <div className={s.biggerStand}>
+          <p className={s.standContent}>
+            In order to build a sumptuous and comfortable seat, VR Racer gaming
+            chair are designed with a continued tradition of race car seat.
+          </p>
+        </div>
       </div>
     </div>
   );

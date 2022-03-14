3c96a923a837dc87d2fe4939fcbcc02f66d74f7e
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-03-14 09:36:52 +0100

    Adds advantages cards to advantages component
    Added cards, svgs, styles to each card

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -15,6 +15,57 @@ const Advantages = () => {
         <div className={s.serviceDesk}></div>
         <div className={s.delivery}></div>
         <div className={s.support}></div>
+
+        <ul className={s.list}>
+          <li className={s.item}>
+            <div className={s.itemWrapper}>
+              <h3 className={s.itemTitle}>lifetime warranty</h3>
+              <div className={s.safetyAdvantages}>
+                <SvgIcon iconName="icon-safety-outlined" />
+              </div>
+              <p className={s.itemText}>
+                We are offering the best warranty for gaming chair Adventurer
+                you can't find anywhere else in the world.
+              </p>
+            </div>
+          </li>
+          <li className={s.item}>
+            <div className={s.itemWrapper}>
+              <h3 className={s.itemTitle}>fast and free delivery</h3>
+              <div className={s.safetyAdvantages}>
+                <SvgIcon iconName="icon-delivery-truck-outlined" />
+              </div>
+              <p className={s.itemText}>
+                Don't want to pay extra shipping when buy gaming chairs online?
+                No problem, we cover it for you.
+              </p>
+            </div>
+          </li>
+          <li className={s.item}>
+            <div className={s.itemWrapper}>
+              <h3 className={s.itemTitle}>excellent customer service</h3>
+              <div className={s.safetyAdvantages}>
+                <SvgIcon iconName="icon-service-desk-outlined" />
+              </div>
+              <p className={s.itemText}>
+                Our live support are real humans standing by ready to help you
+                with all your needs
+              </p>
+            </div>
+          </li>
+          <li className={s.item}>
+            <div className={s.itemWrapper}>
+              <h3 className={s.itemTitle}>support gamers</h3>
+              <div className={s.safetyAdvantages}>
+                <SvgIcon iconName="icon-customer-service-outlined" />
+              </div>
+              <p className={s.itemText}>
+                We are offering the best warranty for gaming chair Adventurer
+                you can't find anywhere else in the world.
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
